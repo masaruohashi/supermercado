c7fa48cc085ccf852473ad2e4194ab2120038be8
FelipeFL96 <felipe-fernandes96@hotmail.com>
2017-12-11 22:01:08 -0200

    Correções de código

--- src/com/supermercado/dao/SellersDAO.java
+++ src/com/supermercado/dao/SellersDAO.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.supermercado.models.Client;
 import com.supermercado.models.Seller;
 
 public class SellersDAO extends BaseDAO{
--- src/com/supermercado/jdbc/ConnectionFactory.java
+++ src/com/supermercado/jdbc/ConnectionFactory.java
@@ -9,10 +9,11 @@ public class ConnectionFactory {
     Connection connection = null;
     try {
       Class.forName("com.mysql.jdbc.Driver");
-      connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/supermercado", "root", "960403");
+      connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/supermercado", "root", "");
     } catch (SQLException | ClassNotFoundException e) {
       e.printStackTrace();
     }
     return connection;
   }
 }
+	
